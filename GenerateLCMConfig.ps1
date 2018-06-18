89f703b0a60bd65b4ad5707e06e3416bde9373fa
fitzwar <39277560+fitzwar@users.noreply.github.com>
2018-06-18 15:56:22 +0100

    Working script, First push
    Working script, First push

---
+++ GenerateLCMConfig.ps1
@@ -0,0 +1,26 @@
+﻿####This configures the LCMConfig
+##
+configuration LCMConfig {
+#parameters
+    param([string[]]$computername = "localhost")
+#Target Node
+    node $computername {
+    #LCM Resource
+    Localconfigurationmanager {
+        ConfigurationMode = "ApplyAndAutoCorrect"
+        ConfigurationModeFrequencyMins = 30
+        }
+       }
+      }
+
+      # Generate mof file
+      LCMConfig -computername localhost
+
+      #Check LCM Settings on targe node
+      Get-DscLocalConfigurationManager -CimSession localhost
+
+      #Apply the LCMConfig for each targe node
+      Set-DscLocalConfigurationManager -path LCMConfig
+
+      #Check the LCM Config again
+      Get-DscLocalConfigurationManager -CimSession localhost
